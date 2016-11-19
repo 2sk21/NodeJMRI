a56b0587e7d202409fa2613c9337a8873ff7eede
Rangachari Anand <ranand@us.ibm.com>
2016-11-19 14:04:34 -0500

    Initial checkin - code does not run. Try to figure out why connection setup fails

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NodeJMRI
 Access Java Model Railroad Interface via websocket
+This is work in progress will not work
+
+Instructions
+------------
+
+* To install:
+** npm install .
+* To run 9replace with your IP address)
+** node index.js ws://10.0.1.101:2056/json
---
+++ index.js
@@ -0,0 +1,14 @@
+
+var WebSocketClient = require('websocket').client;
+ 
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+});
+
+client.connect(process.argv[2], 'jmri');
